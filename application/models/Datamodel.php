b7bd59bfc667228ad12df7379c2ab0d0485b4c7e
Zhou Wei <zhouwei1026@gmail.com>
2016-05-11 21:59:59 -0400

    Update https connection with lrs

--- application/models/Datamodel.php
+++ application/models/Datamodel.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class DataModel extends CI_Model{
-	private $pipeline_url = "http://lrs.keep.edu.hk/public/api/v1/statements/aggregate?pipeline=";
+	private $pipeline_url = "https://lrs.keep.edu.hk/api/v1/statements/aggregate?pipeline=";
 	private $auth = array(
 		"moodle" => "ZGVmYTRiMjM5ODFhY2Q5YmRkNGU0OGM4N2I3NGU1NDhmYmNiNjEwYzplMzAwOWJkM2RiNTZiMTNmMjg5ZGQ2YTM2M2Y4MWQ3OGY4OTdkMzVm",
 		"edx" => "Y2M5ZTQ0YmUwMmE2NTg4MTRmNDJlMmZmNDI1ODBkYjE3ZGVmMjMyMDo3ZGQyMzk5MjhmNmZkMzIyNjFjODgzODM1MmI0YjA4ZDQ5NzQ1Mjk3",
