af7a2b579d406f213e48fd42fd984baaa2d54bcf
Zhou Wei <zhouwei1026@gmail.com>
2016-05-17 16:59:25 +0800

    daily_statistic function Done!

--- application/models/Cachemodel.php
+++ application/models/Cachemodel.php
@@ -1,9 +1,9 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class DataModel extends CI_Model{
+class CacheModel extends CI_Model{
 
-  private engagement_list;
+  private $engagement_list;
 
   public function __construct(){
     parent::__construct();
@@ -34,8 +34,110 @@ class DataModel extends CI_Model{
   }
 
   ////// Daily job //////
-  public function daily_statistic($value='')
-  {
-    # code...
+  //If date is not specified, then this function will return all of the statements.
+  //If date is specified, then this function will only return the data in that perticular day.
+  public function daily_statistic($date = null){
+    $this->load->model('datamodel');
+    $this->load->model('courseinfomodel');
+    $statistic_records = array();
+
+    //Load course list making use of Courseinfo Model 
+    $courseList = $this->courseinfomodel->getData(array('moodle' => '/metric/course', 'edx' => '/metric/course'));
+    if(array_key_exists('results', $courseList['data']['moodle'])){
+      foreach($courseList['data']['moodle']['results'] as $value){
+        //Get all of the statements
+        $output = $this->getDataFromDataModel('moodle', $value['id'], $date);
+        //Rearrange the structure of the data.
+        foreach($output['data']['moodle']['result'] as $record){
+          $statistic_records[] = array(
+            "course_id" => $value['id'],
+            "course_name" => $value['fullname'],
+            "platform" => "moodle",
+            "statement" => $record['_id']['Engagement'],
+            "date" => $record['_id']['date'],
+            "count" => $record['count'],
+          );
+        }        
+      }
+    }
+    if(array_key_exists('results', $courseList['data']['edx'])){
+      foreach($courseList['data']['edx']['results'] as $value){
+        $output = $this->getDataFromDataModel('edx', $value['course_id'], $date);
+        foreach($output['data']['edx']['result'] as $record){
+          $statistic_records[] = array(
+            "course_id" => $value['course_id'],
+            "course_name" => $value['course_name'],
+            "platform" => "edx",
+            "statement" => $record['_id']['Engagement'],
+            "date" => $record['_id']['date'],
+            "count" => $record['count'],
+          );
+        }        
+      }
+    }
+    return $statistic_records;
+    
+  }
+
+  function getDataFromDataModel($platform, $courseId, $date = null){
+    $key = getKey($platform);
+    $match = array(
+      "\$match" => array(
+        "statement.context.extensions.".$key.".courseid" => array("\$eq" => $courseId),
+        "statement.context.extensions.".$key.".rolename" => array("\$eq" => "student"),
+        "\$or" => $this->getOrArray()
+      ),
+    );
+    if($date != null){
+      $match['$match']['statement.timestamp'] = array(
+        "\$gte" => $date . "T00:00",
+        "\$lte" => $date . "T23:59",
+      );
+    }
+    $sortDate = array(
+      "\$sort" => array(
+        "statement.timestamp" => -1,
+      )
+    );    
+    $group = array(
+      "\$group" => array(
+        "_id" => array(
+          "Engagement" => array("\$concat" => array("\$statement.verb.display.en-us", " ", "\$statement.object.definition.name.en-us")),
+          "date" => array("\$substr"=>array("\$statement.timestamp", 0, 10,),),
+        ),
+        "count" => array("\$sum" => 1)
+      )
+    );
+    $project = array(
+      "\$project" => array(
+        "_id" => 0,
+        "statement.verb.display.en-us" => 1,
+        "statement.object.definition.name.en-us" => 1,
+        "statement.timestamp" => 1,
+      )
+    );
+    $pipeline[$platform] = array($match, $sortDate, $group);
+    $output = $this->datamodel->getData($pipeline);
+    return $output;
+  }
+
+  function getOrArray() {        
+    $returnArray = array();
+    
+    foreach ($this->engagement_list as $category => $verbStateArray) {
+      foreach($verbStateArray as $verbState) {
+        $verb = $verbState[0];
+        $name = $verbState[1];
+        
+        $returnArray[] = array(
+          "\$and" => array(
+            array("statement.verb.display.en-us" => array("\$eq" => $verb)),
+            array("statement.object.definition.name.en-us" => array("\$eq" => $name)),
+          ) 
+        );
+      }     
+    }   
+    
+    return $returnArray;
   }
 }
