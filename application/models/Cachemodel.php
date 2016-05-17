02cd1a12465ee022a7c1b700574542c30889d67f
Albert Yang <albertyang@keep.edu.hk>
2016-05-17 17:07:47 +0800

    update cache_db

--- application/models/Cachemodel.php
+++ application/models/Cachemodel.php
@@ -3,7 +3,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class DataModel extends CI_Model{
 
-  private engagement_list;
+  private engagement_list = array();
 
   public function __construct(){
     parent::__construct();
@@ -12,9 +12,27 @@ class DataModel extends CI_Model{
   }
 
   ////// Statement Functions //////
+  public function init_statement_db()
+  {
+    // initial the statement table, just be called during deployment
+    $table_name = "dash_statement";
+    foreach ($this->engagement_list as $category => $statement_array) {
+      foreach ($statement_array as $statement) {
+        $statement_data = array(
+          'name' => $statement[0]." ".$statement[1],
+          'category' => $category
+        );
+        $this->db->insert($table_name,$statement_data);
+      }
+    }
+    $this->output['ok'] = true;
+    return $this->output;
+  }
+
   public function insert_statement_db($new_statement)
   {
     # insert a new statement type into database
+
   }
 
   public function delete_statement_db($del_statement)
--- doc/cache_db.sql
+++ doc/cache_db.sql
@@ -1,18 +1,20 @@
 # Group is reserved word in mysql, i changed to category
 CREATE TABLE dash_statement
 (
-id int NOT NULL,
-name varchar(100) NOT NULL,
-category varchar(100) NOT NULL,
-PRIMARY KEY (id)
-);
+`id` int NOT NULL AUTO_INCREMENT,
+`name` varchar(100) NOT NULL,
+`category` varchar(100) NOT NULL,
+`status` INT( 11 ) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'expired or not',
+PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE dash_engage
 (
-course_id int NOT NULL,
-platform varchar(50) NOT NULL,
-statement_id int NOT NULL,
-count int NOT NULL,
-timestamp timestamp NOT NULL,
-FOREIGN KEY (statement_id) REFERENCES dash_statement(id)
-);
+`course_id` int NOT NULL,
+`platform` varchar(50) NOT NULL,
+`statement_id` int NOT NULL,
+`count` int NOT NULL,
+`timestamp` timestamp NOT NULL,
+PRIMARY KEY (`course_id`,`platform`),
+FOREIGN KEY (`statement_id`) REFERENCES `dash_statement`(`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
