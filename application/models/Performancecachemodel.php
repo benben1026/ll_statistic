76bd618394e108d1f14421aeb44744af2b2bb30e
kenhui <skkhui@keep.edu.hk>
2016-05-17 09:45:33 +0800

    Added Perf cache model for db data query

---
+++ application/models/Performancecachemodel.php
@@ -0,0 +1,11 @@
+<?php
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class PerformanceCacheModel extends CI_Model{
+    
+    // pass the courseID and date, and data type wanted to retrieve the data
+    
+    // pass the courseID to retrieve the data
+    
+}
