47f564861a62c4b0e3e4e68db2d184bd03e1c485
kenhui <skkhui@keep.edu.hk>
2016-05-25 08:57:17 +0800

    formatted cahche_db.sql

--- doc/cache_db.sql
+++ doc/cache_db.sql
@@ -5,22 +5,22 @@ DROP TABLE dash_statement;
 
 CREATE TABLE dash_statement
 (
-`statement_id` int NOT NULL AUTO_INCREMENT,
-`name` varchar(100) NOT NULL,
-`category` varchar(100) NOT NULL,
-`updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (`statement_id`)
+    `statement_id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `category` varchar(100) NOT NULL,
+    `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`statement_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE dash_engage
 (
-`engagement_id` int NOT NULL AUTO_INCREMENT,
-`course_id` varchar(200) NOT NULL,
-`platform` varchar(50) NOT NULL,
-`statement_id` int NOT NULL,
-`statement_count` int NOT NULL,
-`statistic_date` DATE NOT NULL,
-`updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (`engagement_id`),
-FOREIGN KEY (`statement_id`) REFERENCES `dash_statement`(`statement_id`)
+    `engagement_id` int NOT NULL AUTO_INCREMENT,
+    `course_id` varchar(200) NOT NULL,
+    `platform` varchar(50) NOT NULL,
+    `statement_id` int NOT NULL,
+    `statement_count` int NOT NULL,
+    `statistic_date` DATE NOT NULL,
+    `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`engagement_id`),
+    FOREIGN KEY (`statement_id`) REFERENCES `dash_statement`(`statement_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
