77ffa9326afccd26ac5f4e8d3a390951c79742c6
Albert Yang <albertyang@keep.edu.hk>
2016-05-31 10:34:36 +0800

    update cron_job to ouput log

--- doc/cron_job.sh
+++ doc/cron_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 now=$(date -d "yesterday" +"%Y-%m-%d")
-echo -e "\n\n###### $now ######" >> cron.log
+echo -e "\n\n###### `date` ######" >> cron.log
 curl "https://dash.keep.edu.hk/index.php/engagement/cron_job?date=$now" >> cron.log 2>&1
